9be93f81c005049c948fdfe1952a2ac2b0c3b126
Samuel Soares de Souza <149734741+SamuelSoaresDev@users.noreply.github.com>
2025-01-23 23:22:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 let jogadas = 4;
 
-
+reiniciarJogo();
 
 function nivel() {
-  reiniciarJogo();
+  
   var dificuldade = prompt(
     "Escolha a dificuldade do jogo\nDigite o número referente à dificuldade: \n1 - Fácil\n2 - Médio\n3 - Difícil"
   );
